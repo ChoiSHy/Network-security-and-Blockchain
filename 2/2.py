86277cce4c3b10bcb7cc86272e5b4872eb68f91e
ChoiSHy <tjdgus4697@gmail.com>
2023-03-14 04:18:20 +0900

    2번 하는중, 14일 오전4시2번 하는중, 14일 오전4시

--- 2/2.py
+++ 2/2.py
@@ -4,23 +4,7 @@ import time
 import struct
 import math
 
-def euclid(a, b):
-    if b == 0:
-        return a, 1, 0
-    else:
-        gcd, i, j = euclid(b, a % b)
-        x = j
-        y = i - (a//b) * j
-        return gcd, x, y
-
-def mod_inverse(a, b):
-    gcd, x, y = euclid(a, b)
-    if gcd == 1:
-        return x % b
-    else:
-        return 0
-
-
+# 키 생성 함수
 def make_key(p):
     key_bytes = struct.pack('>d', random.random()) + struct.pack('>d', time.time()*1000) + os.urandom(16)
     private_key = int.from_bytes(key_bytes)
@@ -28,20 +12,54 @@ def make_key(p):
         private_key = int.from_bytes( struct.pack('>d', random.random()) + struct.pack('>d', time.time()*1000) + os.urandom(16))
     return private_key
 
-def ellipticCurve_points(p):
-    x = 0
-    while x < p :
-        w = (x**3+7) % p
+def exEuclidian(n, b):
+    r1=n   
+    r2=b    
+    t1=0    
+    t2=1
 
-        if int(w**0.5)== w**0.5:
-            return (x,w**(0.5))
-        x+=1
-
-
-
-
-p = int(0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F)
-key= make_key(p)
-
-print(ellipticCurve_points(p))        
+    while r2 > 0:
+        q = r1 // r2
  
+        r = r1 - q * r2
+        r1 = r2
+        r2 = r
+
+        t = t1 - q * t2
+        t1 = t2
+        t2 = t
+    
+    return t1 if t1 >= 0 else n+t1
+
+def add(x1, y1, x2, y2, p):
+    if x1 == x2 & y1 == y2:
+        r = ((3 * x**2 ) * exEuclidian(p, 2 * y1)) % 13
+    else:
+        r = ((y2-y1) * exEuclidian(p, x2 - x1)) % 13
+
+    x = (r**2 - x1 - x2) % p
+    y = (r * (x1 - x) -y1) % p
+
+    return x, y
+
+def daa(x, y, k, p):
+    if k=='1':
+        tx, ty = add(x, y, x, y, p)
+        rx, ry = add(tx, ty ,x, y, p)
+    else:
+        rx, ry = add(x,y,x,y,p)
+    return rx, ry 
+
+p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
+gx = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798
+gy = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
+
+k = 0x771ab89947b6e39e1aaa7610085e5657e1eef2da7ccdf7af7d35b0413e661d38
+k = format(k,'b')
+for i in range(1,len(k)):
+    gx,gy = daa(gx,gy, k[i], p)
+    
+print(hex(gx))
+print(hex(gy))
+
+# key= make_key(p)
