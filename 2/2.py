18802dcf95c66d761225208173d1ddcb1d59ad10
ChoiSHy <tjdgus4697@gmail.com>
2023-03-21 15:00:36 +0900

    임시

--- 2/2.py
+++ 2/2.py
@@ -5,8 +5,6 @@ import struct
 import math
 
 # 키 생성 함수
-
-
 def make_key(p):
     key_bytes = struct.pack('>d', random.random()) + \
         struct.pack('>d', time.time()*1000) + os.urandom(16)
@@ -16,7 +14,7 @@ def make_key(p):
             '>d', random.random()) + struct.pack('>d', time.time()*1000) + os.urandom(16))
     return private_key
 
-
+# 확장 유클리드드
 def exEuclidian(n, b):
     r1 = n
     r2 = b
@@ -36,13 +34,15 @@ def exEuclidian(n, b):
 
     return t1%n
 
-
+# 두 점 덧셈(= add)
 def add(x1, y1, x2, y2, p):
     r = ((y2-y1) * exEuclidian(p, (x2 - x1)%p)) % p
     x = (r**2 - x1 - x2) % p
     y = (r * (x1 - x) - y1) % p
 
     return x, y
+
+# 접점 덧셈(= double) 
 def dadd(x1,y1,p):
     r = (3*(x1**2) * exEuclidian(p,(2*y1)%p))%p
     x = (r**2 - x1 - x1) % p
@@ -54,8 +54,9 @@ def dadd(x1,y1,p):
 p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
 x = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798
 y = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
-k = 0x771ab89947b6e39e1aaa7610085e5657e1eef2da7ccdf7af7d35b0413e661d38
-
+#k = 0x771ab89947b6e39e1aaa7610085e5657e1eef2da7ccdf7af7d35b0413e661d38
+k = make_key(p)
+print('k: ', hex(k))
 k = format(k, 'b')
 
 res_x = x
@@ -66,7 +67,6 @@ for i in range(1, len(k)):
         res_x, res_y = add(res_x, res_y, x, y, p)
     else:
         res_x, res_y = dadd(res_x, res_y, p)
+print('x: ', res_x)
+print('y: ', res_y)
 
-print('x: ', hex(res_x))
-print('y: ', hex(res_y))
-# key= make_key(p)
